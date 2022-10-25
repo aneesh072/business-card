238b5f2054b57ab7c7ff3c41ee298b2f37002773
aneesh072 <aneeshkarki292@gmail.com>
2022-10-25 18:01:58 +1100

    created useTheme custome hook to load the form from theme.json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import * as themes from './theme/schema.json';
+import { setToLS } from './utils/storage';
+
+const Index = () => {
+  setToLS('all-themes', themes.default);
+  return <App />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<Index />);
